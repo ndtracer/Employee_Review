fbc6bec9287646cb377cb90fa85034c030ed5e2f
ndtracer <tracejanota@gmail.com>
2023-10-04 12:16:07 -0500

    Changed a lot. Navbar component design set

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EmployeeReviewComponent } from './employee-review/employee-review.component';
+import { SharedComponent } from './shared/shared.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeReviewComponent,
+    SharedComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/employee-review/employee-review.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-review',
+  templateUrl: './employee-review.component.html',
+  styleUrls: ['./employee-review.component.css']
+})
+export class EmployeeReviewComponent {
+  titleId = 'Worksite'
+}
