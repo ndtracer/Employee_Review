16a3c7f09bc2942da37ce52adf5d37590873fdde
ndtracer <tracejanota@gmail.com>
2023-11-16 06:35:54 -0600

    Initial Firebase Setup

--- src/app/_helpers/http.service.ts
+++ src/app/_helpers/http.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@angular/core';
-import { HttpRequest, HttpResponse, HttpHandler, HttpEvent, HttpInterceptor, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClient, HttpRequest, HttpResponse, HttpHandler, HttpEvent, HttpInterceptor, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { Observable, of, throwError } from 'rxjs';
 import { delay, materialize, dematerialize } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: "root"
+})
+export class HTTPService {
+  // Variables
+  firebaseRootURL = "https://employee-review-app-1-default-rtdb.firebaseio.com/";
+
+  constructor(
+    private http: HttpClient,
+
+  ) {}
+
+}
